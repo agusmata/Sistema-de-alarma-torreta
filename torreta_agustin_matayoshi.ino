2bc9c88fd4fed795cbf8d956a067f1fafc4e4808
Agustin Matayoshi <agus.matayoshi@gmail.com>
2023-04-26 18:12:45 -0300

    subida ultima version del script

--- torreta_agustin_matayoshi.ino
+++ torreta_agustin_matayoshi.ino
@@ -1,5 +1,7 @@
+#include <Servo.h>
 #include <LiquidCrystal.h>
 #include <Keypad.h>
+
   const byte ROWS = 4; //Cuatro filas
   const byte COLS = 3; //Tres columnas
   char keys[ROWS][COLS] = {
@@ -8,14 +10,16 @@
     {'7','8','9'},
     {'*','0','#'}
   };
-  byte rowPins[ROWS] = {9, A5, A4, A3}; //pines que se utilizan para las filas del keypad
-  byte colPins[COLS] = {A2, A1, A0}; //pines que se utilizan para las columnas del keypad
+  byte rowPins[ROWS] = {28, 27, 26, 25}; //pines que se utilizan para las filas del keypad
+  byte colPins[COLS] = {24, 23, 22}; //pines que se utilizan para las columnas del keypad
   Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
-  LiquidCrystal lcd(12,11,5,4,3,2);
+  LiquidCrystal lcd(38,37,36,35,34,33);
+  Servo AlaIzq;
+  Servo AlaDer;
   byte control=0, pad[4]={0,0,0,0}, contra[4]={'1','5','9','3'};//se utiliza una variable para que se guarden los datos del pad y otra para comparar
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-  bool torretaACT=true, banderapir=LOW, alarma=LOW, parpadeo=HIGH;
-  byte pir=10, buzz=8, ledR=6, ledV=7;
+  bool torretaACT=true, banderapir=LOW, alarma=LOW, parpadeo=HIGH, carga=HIGH;
+  byte pir=10, buzz=30, ledR=6, ledV=7, ledR2=3, ledV2=4;
   byte cursorlcd=0, pos=0;
   unsigned long tiempo=0, pausa=0;
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -24,9 +28,13 @@ void setup() {
   lcd.begin(16,2);
   Serial.begin(9600);
   pinMode(pir,INPUT);
-  for(byte x=6;x<9;x++){
-    pinMode(x,OUTPUT);
-  }
+  pinMode(buzz,OUTPUT);
+  pinMode(ledR,OUTPUT);
+  pinMode(ledV,OUTPUT);
+  pinMode(ledR2,OUTPUT);
+  pinMode(ledV2,OUTPUT);
+  AlaDer.attach(11);
+  AlaIzq.attach(8);
  }
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -36,14 +44,27 @@ void loop() {
 //////////////////////////////////////////////////////
   if ((torretaACT==true) && (alarma==LOW)){
     estpir=digitalRead(pir);
+    lcd.setCursor(0,0);     
+    if (carga==LOW){
+      
+      for(byte x=0;x<10;x++){
+      digitalWrite(buzz,HIGH);
+      delay(500);
+      digitalWrite(buzz,LOW);
+      delay(500);
+      }
+      carga=HIGH;
+    }
     lcd.setCursor(0,0);
     lcd.print("Alarma On     ");
      if (millis()>pausa+500){
        digitalWrite(ledR,parpadeo);
+       digitalWrite(ledR2,parpadeo);
        parpadeo=!parpadeo;
        pausa=millis();
      }
-    digitalWrite(ledV,LOW); 
+    digitalWrite(ledV,LOW);
+    digitalWrite(ledV2,LOW); 
   }
 
   if (torretaACT==false){
@@ -51,7 +72,12 @@ void loop() {
     lcd.setCursor(0,0);
     lcd.print("Alarma Off      ");
     digitalWrite(ledR,LOW);
-    digitalWrite(ledV,HIGH); 
+    digitalWrite(ledR2,LOW);
+    digitalWrite(ledV,HIGH);
+    digitalWrite(ledV2,HIGH); 
+    AlaIzq.write(180);
+    AlaDer.write(180);
+    delay(10);
   }
   Serial.println(estpir);
 //////////////////////////////////////////////////////
@@ -70,21 +96,37 @@ void loop() {
      delay(50);
      digitalWrite(buzz,LOW);
      delay(50);
-     digitalWrite(ledR,HIGH); 
+     digitalWrite(ledR,HIGH);
+     digitalWrite(ledR2,HIGH); 
      digitalWrite(ledV,LOW);
+     digitalWrite(ledV2,LOW);
      tiempo=millis();
      banderapir=estpir;
      alarma=HIGH; 
-     
+     carga=LOW;
+     AlaIzq.write(90);
+     AlaDer.write(90);
+     delay(10);
   }
 
   if ((millis()>tiempo+7000)&&(alarma==HIGH)){
     alarmaactivada();
+    
+  
+    
   }
 //////////////////////////////////////////////////////////////////////////////////////
   if (pos<4){
     if (key != NO_KEY){//entrada de datos para el keypad
+
       if (key=='#'){
+        tone(buzz,4275);
+        delay(50);
+        noTone(buzz);
+        delay(50);
+        tone(buzz,4275);
+        delay(50);
+        noTone(buzz);
         lcd.clear();
         lcd.setCursor(0,0);
         lcd.print("Ingrese la clave");
@@ -93,6 +135,9 @@ void loop() {
         cursorlcd=0;
       }
       else{
+            tone(buzz,4275);
+            delay(50);
+            noTone(buzz);
             lcd.setCursor(cursorlcd,1);
             //lcd.print(key);
             lcd.print("*");
@@ -119,8 +164,10 @@ void loop() {
       cursorlcd=0;
       alarmaactivada();
       tiempo=0;
-      digitalWrite(ledR,HIGH); 
-      digitalWrite(ledV,LOW); 
+      digitalWrite(ledR,HIGH);
+      digitalWrite(ledR2,HIGH); 
+      digitalWrite(ledV,LOW);
+      digitalWrite(ledV2,LOW); 
     }        
     if(control==4){//en caso de ser correcta se desactiva la alarma
       lcd.setCursor(0,0);
